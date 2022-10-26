ea0d734aedb2b61a8b2865ee92836deb99bdca56
Md.kamruzzaman Rubel <rubelrk377@gmail.com>
2022-10-26 18:02:26 +0600

    create server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Port = process.env.Port || 5000;
 const allCourses = require("./Data/product.json");
 
 app.get("/", (req, res) =>{
-    res.send("No data Available");
+    res.send("No Data Available");
 });
 app.get("/allCourses", (req, res) =>{
     res.send(allCourses);
@@ -17,14 +17,14 @@ app.get("/course/:id", (req, res) =>{
     const id = req.params.id;
     const singleCourse = allCourses?.find((c) => c.id == id);
     if(!singleCourse){
-        res.send("No data found");
+        res.send("No Data Found");
     }
 
     res.send(singleCourse);
 });
 
 app.listen(Port, () =>{
-    console.log("server is running",Port);
+    console.log("Server Is Running",Port);
 });
 
 module.exports = app;
